20cc1734dd4474bd4351bf686d1525797bf6a0a1
Tasos <anasgout@outlook.com>
2024-05-04 03:37:22 +0200

    init

---
+++ src/main/java/com/example/SubshellZoo/SubshellZooApplication.java
@@ -0,0 +1,13 @@
+package com.example.SubshellZoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubshellZooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubshellZooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SubshellZoo/SubshellZooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SubshellZoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubshellZooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
